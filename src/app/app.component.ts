f05ad97fa820fcec4fe0c9b252299090098db5a8
Maciej Wojcik <maciejwwojcik@gmail.com>
2022-11-09 22:11:52 +0100

    Image Optimization App initial app implementation - no optimization at all - added 15 images, 1 hero, and 14 content images, aprox 1MB per image, each 1920px width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'image-optimization';
+
+  readonly heroImage: string = 'assets/pizza-hero.jpg';
+  readonly images: string[] = [
+    'assets/pizza-1.jpg',
+    'assets/pizza-2.jpg',
+    'assets/pizza-3.jpg',
+    'assets/pizza-4.jpg',
+    'assets/pizza-5.jpg',
+    'assets/pizza-6.jpg',
+    'assets/pizza-7.jpg',
+    'assets/pizza-8.jpg',
+    'assets/pizza-9.jpg',
+    'assets/pizza-10.jpg',
+    'assets/pizza-11.jpg',
+    'assets/pizza-12.jpg',
+    'assets/pizza-13.jpg',
+    'assets/pizza-14.jpg',
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgOptimizedImage } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
