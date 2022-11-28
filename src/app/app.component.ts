902b7c7db8a23421c5e4967fbdc652e907ee6328
Maciej Wojcik <maciejwwojcik@gmail.com>
2022-11-28 17:34:19 +0100

    changed images to be served from vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  readonly heroImage: string = 'assets/pizza-hero.jpg';
+  readonly heroImage: string = 'https://image-optimization-app.vercel.app/assets/pizza-hero.jpg';
   readonly images: string[] = [
-    'assets/pizza-1.jpg',
-    'assets/pizza-2.jpg',
-    'assets/pizza-3.jpg',
-    'assets/pizza-4.jpg',
-    'assets/pizza-5.jpg',
-    'assets/pizza-6.jpg',
-    'assets/pizza-7.jpg',
-    'assets/pizza-8.jpg',
-    'assets/pizza-9.jpg',
-    'assets/pizza-10.jpg',
-    'assets/pizza-11.jpg',
-    'assets/pizza-12.jpg',
-    'assets/pizza-13.jpg',
-    'assets/pizza-14.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-1.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-2.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-3.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-4.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-5.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-6.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-7.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-8.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-9.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-10.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-11.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-12.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-13.jpg',
+    'https://image-optimization-app.vercel.app/assets/pizza-14.jpg',
   ];
 
 }
