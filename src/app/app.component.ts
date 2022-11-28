3ec9d1f03e5b38d3d2b7af74266561693c41a2e8
Maciej Wojcik <maciejwwojcik@gmail.com>
2022-11-28 16:38:11 +0100

    test with assets images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  readonly heroImage: string = 'https://i.postimg.cc/4dzWksCv/pizza-hero.jpg';
+  readonly heroImage: string = 'assets/pizza-hero.jpg';
   readonly images: string[] = [
-    'https://i.postimg.cc/gc7WkH1x/pizza-1.jpg',
+    'assets/pizza-1.jpg',
     'https://i.postimg.cc/P5WsysJD/pizza-2.jpg',
     'https://i.postimg.cc/rwnbQVcK/pizza-3.jpg',
     'https://i.postimg.cc/YSvPP5q8/pizza-4.jpg',
